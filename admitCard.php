b7cf379d9a398ce557c199201b132508a1d3e27a
vimal saini <vimal.calinfo@gmail.com>
2024-05-30 16:57:27 +0530

    admit id card with two images
    admit card and id card

--- admitCard.php
+++ admitCard.php
@@ -338,10 +338,10 @@
     <div class="page-banner-area bg-2">
         <div class="container">
             <div class="page-banner-content">
-                <h1>Student ID card</h1>
+                <h1>Admit card</h1>
                 <ul>
                     <li><a href="index.php">Home</a></li>
-                    <li>Student ID card</li>
+                    <li>Admit ID card</li>
                 </ul>
             </div>
         </div>
@@ -350,30 +350,27 @@
 
     <div class="login-area pt-100 pb-70">
         <div class="container">
-        <div class="row">
-            <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="login">
+                        <h3>Admit Card</h3>
+                        <form>
+                        <div class="form-group">
+                                <input type="text" id="name" class="form-control" placeholder="User Name*" required>
+                            </div> 
+                            <div class="form-group">
+                                <input type="text" id="registration" class="form-control" placeholder="Registration" required>
+                            </div>                                                    
+                            <button type="submit" name="check" class="default-btn btn active">Check</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-6">
             <div class="login">              
-           <img src="assets/images/newimages/idcard.jpg" alt="">
+           <img src="assets/images/newimages/admit.jpg" alt="">
             </div>
             </div>
-            <div class="col-md-6">
-            <div class="login">
-                <h3>Student  ID card</h3>
-                <form>
-                    <div class="form-group">
-                        <input type="text" id="name" class="form-control" placeholder="Username">
-                    </div>
-                    <div class="form-group">
-                        <input type="email" id="email" class="form-control" placeholder="Email Address*">
-                    </div>
-                    <div class="form-group">
-                        <input type="date" id="dob" class="form-control" placeholder="Date of Birth*">
-                    </div>
-                    <button type="submit"  name="check" class="default-btn btn active">Check</button>                  
-                </form>
             </div>
-            </div>
-        </div>           
         </div>
     </div>
 
